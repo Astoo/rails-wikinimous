694600f9e263aa7b764289531c3f7021e1acfd79
Louis Fanien <louis.fanien@gmail.com>
2017-08-08 15:31:42 +0200

    before optionals

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,8 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+10.times do
+Article.create(title: Faker::ChuckNorris.fact, content: Faker::HowIMetYourMother.quote)
+end
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
